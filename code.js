94bb2b784d5fc75fb3ba8e634d2a125032db5b1a
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-08-28 23:11:26 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,2 +1,9 @@
-for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
-for (var i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
+// ==UserScript==
+// @name         youtube-smallerThumbnails
+// @match        https://www.youtube.com/*
+// ==/UserScript==
+
+window.onscroll = function (e) {
+    for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
+    for (var i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
+}
