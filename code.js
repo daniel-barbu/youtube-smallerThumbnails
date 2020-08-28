707a175087b97ce869102d526797882fb49b716b
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-08-28 23:18:27 +0300

    Update code.js

--- code.js
+++ code.js
@@ -3,6 +3,9 @@
 // @match        https://www.youtube.com/results?search_query=*
 // ==/UserScript==
 
+for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
+for (var i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
+
 window.onscroll = function (e) {
     for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
     for (var i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
