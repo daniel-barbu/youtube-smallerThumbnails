0ec7843d8b6427903913b130fbea20a6a5bb253a
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-08-28 22:43:14 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,2 +1,2 @@
-for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) 
-  document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
+for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
+for (var i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
