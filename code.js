324f4c76576b51402078c62dc6a914140a002c05
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-09-03 12:13:27 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         youtube-smallerThumbnails
-// @match        https://www.youtube.com/results?search_query=*
+// @match        *.youtube.com/*
 // ==/UserScript==
 
 if (window.location.href.startsWith("https://www.youtube.com/results?search_query=")) {
-    for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
-    for (var i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
-
-    window.onscroll = function (e) {
-        for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
-        for (var i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
-    }
+    function fct() {
+        console.log("exec");
+        for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) {
+            document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
+            document.getElementsByTagName("ytd-thumbnail")[i].childNodes[2].childNodes[3].style.zoom="1.3";
+            document.getElementsByTagName("ytd-thumbnail")[i].childNodes[2].childNodes[7].style.zoom="1.2";
+        }
+        for (i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
+    } fct();
+    window.onscroll=function(){fct()};
 }
