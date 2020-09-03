59ae56456a214d68159e57b25abb9eab4139d094
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-09-03 11:25:49 +0300

    Update code.js

--- code.js
+++ code.js
@@ -3,10 +3,12 @@
 // @match        https://www.youtube.com/results?search_query=*
 // ==/UserScript==
 
-for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
-for (var i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
-
-window.onscroll = function (e) {
+if (window.location.href.startsWith("https://www.youtube.com/results?search_query=")) {
     for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
     for (var i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
+
+    window.onscroll = function (e) {
+        for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
+        for (var i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
+    }
 }
