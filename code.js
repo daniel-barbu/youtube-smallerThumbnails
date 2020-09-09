fd508a5eb8127b2393950eb83a4a571c02a2a723
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-09-10 02:38:39 +0300

    Update code.js

--- code.js
+++ code.js
@@ -5,7 +5,7 @@
 
 if (window.location.href.startsWith("https://www.youtube.com/results?search_query=")) {
     function fct() {
-        console.log("exec");
+        console.log("exec fct()");
         for (var i=0; i<document.getElementsByTagName("ytd-thumbnail").length; i++) {
             document.getElementsByTagName("ytd-thumbnail")[i].style.zoom="0.8";
             document.getElementsByTagName("ytd-thumbnail")[i].childNodes[2].childNodes[3].style.zoom="1.3";
@@ -13,5 +13,5 @@ if (window.location.href.startsWith("https://www.youtube.com/results?search_quer
         }
         for (i=0; i<document.getElementsByTagName("ytd-playlist-thumbnail").length; i++) document.getElementsByTagName("ytd-playlist-thumbnail")[i].style.zoom="0.8";
     } fct();
-    window.onscroll=function(){fct()};
 }
+window.onscroll=function(){fct()};
